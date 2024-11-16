4407e91e19ae3d8ec358577f75537f0f86b3a013
jeongbaebang <jeongbaebang_dev@naver.com>
2024-11-16 20:37:26 +0900

    5

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -11,7 +11,8 @@ export default function LoginForm() {
   return (
     <form className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className={`${Lusitana.className} mb-3 text-2xl`}>
+        {/* ${Lusitana.className} */}
+        <h1 className={`mb-3 text-2xl`}>
           Please log in to continue.
         </h1>
         <div className="w-full">
