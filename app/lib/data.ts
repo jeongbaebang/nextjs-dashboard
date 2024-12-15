d63e8127e72780def40be8907a2815b38a44d85c
jeongbaebang <jeongbaebang_dev@naver.com>
2024-12-15 16:14:38 +0900

    12 - 13 - 14

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -86,7 +86,7 @@ export async function fetchCardData() {
 const ITEMS_PER_PAGE = 6;
 export async function fetchFilteredInvoices(
   query: string,
-  currentPage: number,
+  currentPage: number
 ) {
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
@@ -158,6 +158,7 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
+    console.log(invoice);
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
